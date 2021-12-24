90a011d2ec25c91c6f0cd227b4e84dc05217643c
svetlana030703 <91036531+svetlana030703@users.noreply.github.com>
2021-12-24 14:45:59 +1000

    Add files via upload

---
+++ 1-6.py
@@ -0,0 +1,7 @@
+n = int( input())
+
+def f(n):
+    return n**2
+    
+
+print('Квадрат числа равен:', f(n))
---
+++ 2-6.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+def f(n):
+    n1 = n-1
+    n2 = n+1
+    return [n1, n2]
+    
+print (f(n))
